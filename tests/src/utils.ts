87211e5b2fdc6bf4c2c9e35506eba1c9a9847496
Eyal Chojnowski <eyal.choj@gmail.com>
2023-11-07 17:21:55 +0100

    :recycle: tests: Dirty update src/utils.ts

--- tests/src/utils.ts
+++ tests/src/utils.ts
@@ -1,51 +1,194 @@
 import { readFileSync } from "node:fs";
 
+import { expect } from "vitest";
 import Arweave from "arweave";
 import { JWKInterface } from "arweave/node/lib/wallet";
 import {
     Contract,
+    ContractError,
+    InteractionResult,
+    Tag,
+    WARP_TAGS,
     Warp,
     WriteInteractionOptions,
     WriteInteractionResponse,
-    WriteInteractionResponseFailure,
-    WriteInteractionResponseSuccess,
+    // WriteInteractionResponseFailure,
+    // WriteInteractionResponseSuccess,
 } from "warp-contracts";
-import { expect } from "@jest/globals";
+import Irys from "@irys/sdk";
 
-import { State as Erc1155State } from "erc1155/State";
+import { Parameters as Erc1155State } from "erc1155/State";
 import { Action as Erc1155Action } from "erc1155/Action";
-import { State as ScarcityState } from "scarcity/State";
-import { State as ShuffleState } from "shuffle/State";
-import { State as LockState } from "lock/State";
+import { ArweaveSigner } from "warp-contracts-plugin-deploy";
+import { Parameters as ScarcityState } from "scarcity/State";
+// import { State as ShuffleState } from "shuffle/State";
+// import { State as LockState } from "lock/State";
+//
+// export const UNIT = 1_000_000;
 
-export const UNIT = 1_000_000;
+// export function expectOk(result: WriteInteractionResponse | null) {
+//     result.
+//     if (result?.type !== "ok") {
+//         console.log("interaction is error:", JSON.stringify(result, undefined, 2));
+//     }
+//
+//     expect(result?.type).toEqual("ok");
+// }
 
-export function expectOk(
-    result: WriteInteractionResponse<unknown> | null,
-): asserts result is WriteInteractionResponseSuccess {
-    if (result?.type !== "ok") {
-        console.log("interaction is error:", JSON.stringify(result, undefined, 2));
+export async function expectError<ERROR>(promise: Promise<unknown>, expectedError?: ERROR) {
+    try {
+        await promise;
+        throw new Error("Promise didn't throw");
+    } catch (error) {
+        expect(error).toBeInstanceOf(ContractError);
+        if (error instanceof ContractError) {
+            expect(error.error).toEqual(expectedError);
+        }
     }
 
-    expect(result?.type).toEqual("ok");
-}
-
-export function expectError<ERROR>(
-    result: WriteInteractionResponse<ERROR> | null,
-    expectedError?: ERROR,
-): asserts result is WriteInteractionResponseFailure<ERROR> {
-    if (result?.type !== "error") {
-        console.log("interaction is ok:", JSON.stringify(result, undefined, 2));
-    }
-
-    expect(result?.type).toEqual("error");
-
-    if (expectedError) {
-        expect((result as WriteInteractionResponseFailure<ERROR>).error).toEqual(expectedError);
-    }
+    // if (result?.type !== "error") {
+    //     console.log("interaction is ok:", JSON.stringify(result, undefined, 2));
+    // }
+    //
+    // expect(result?.type).toEqual("error");
+    //
+    // if (expectedError) {
+    //     expect((result as WriteInteractionResponseFailure<ERROR>).error).toEqual(expectedError);
+    // }
 }
 
 type ContractName = "erc1155" | "scarcity" | "shuffle" | "lock";
+
+export async function deployInitState(arweave: Arweave, wallet: JWKInterface, state: string) {
+    const address = await arweave.wallets.jwkToAddress(wallet);
+    const irys = new Irys({ url: "https://node1.irys.xyz", token: "arweave", key: wallet });
+
+    const balance = await irys.getBalance(address);
+    console.log("irys fund:", balance.toString());
+    // console.log("funding:", await irys.fund(irys.utils.toAtomic(0.2)));
+
+    const tx = await irys.upload(state, {
+        tags: [{ name: "Content-Type", value: "application/json" }],
+    });
+
+    return tx.id;
+}
+
+export async function deployInitTx<T extends ContractName>(
+    warp: Warp,
+    wallet: JWKInterface,
+    contract: T,
+    initState: string,
+) {
+    const wasmDir = `../${contract}/implementation/pkg`;
+    const wasmGluecode = `${wasmDir}/rust-contract.js`;
+    const wasmPath = `${wasmDir}/rust-contract_bg.wasm`;
+
+    // const initStateTx = await deployInitState(warp.arweave, wallet, JSON.stringify(initState));
+    const initStateTx = await warp.arweave.createTransaction({ data: initState }, wallet);
+    await warp.arweave.transactions.sign(initStateTx, wallet);
+    console.log(await warp.arweave.transactions.post(initStateTx));
+    await warp.testing.mineBlock();
+    await warp.testing.mineBlock();
+    await warp.testing.mineBlock();
+    await warp.testing.mineBlock();
+
+    const initStateTxId = initStateTx.id;
+
+    // const deployment = await warp.deploy(
+    //     {
+    //         initStateTx,
+    //         // wallet: new ArweaveSigner(wallet),
+    //         wallet,
+    //         wasmSrcCodeDir: wasmDir,
+    //         wasmGlueCode: wasmGluecode,
+    //         src: readFileSync(wasmPath),
+    //         evaluationManifest: {
+    //             evaluationOptions: {
+    //                 useKVStorage: true,
+    //             },
+    //         },
+    //     } as any,
+    //     true,
+    // );
+
+    const deployment = await warp.deploy(
+        {
+            wallet,
+            initState: "",
+            wasmSrcCodeDir: wasmDir,
+            wasmGlueCode: wasmGluecode,
+            src: readFileSync(wasmPath),
+            tags: [new Tag(WARP_TAGS.INIT_STATE_TX, initStateTxId)],
+            data: {
+                "Content-Type": "text/html",
+                body: "<h1>hello</h1>",
+            },
+            evaluationManifest: {
+                evaluationOptions: {
+                    useKVStorage: true,
+                },
+            },
+        },
+        true,
+    );
+
+    return deployment;
+}
+
+export async function deployMainnetContract<T extends ContractName>(
+    warp: Warp,
+    wallet: JWKInterface,
+    contract: T,
+    initStateTxId: string,
+) {
+    const wasmDir = `../${contract}/implementation/pkg`;
+    const wasmGluecode = `${wasmDir}/rust-contract.js`;
+    const wasmPath = `${wasmDir}/rust-contract_bg.wasm`;
+
+    // const initStateTx = await deployInitState(warp.arweave, wallet, JSON.stringify(initState));
+    // const initStateTx = "IMgRH_JDL9vw2XlM2pvBnPOYkgR9q9Lfncg2irqg050";
+
+    // const deployment = await warp.deploy(
+    //     {
+    //         initStateTx,
+    //         // wallet: new ArweaveSigner(wallet),
+    //         wallet,
+    //         wasmSrcCodeDir: wasmDir,
+    //         wasmGlueCode: wasmGluecode,
+    //         src: readFileSync(wasmPath),
+    //         evaluationManifest: {
+    //             evaluationOptions: {
+    //                 useKVStorage: true,
+    //             },
+    //         },
+    //     } as any,
+    //     true,
+    // );
+
+    const deployment = await warp.deploy(
+        {
+            wallet,
+            initState: "",
+            wasmSrcCodeDir: wasmDir,
+            wasmGlueCode: wasmGluecode,
+            src: readFileSync(wasmPath),
+            tags: [new Tag(WARP_TAGS.INIT_STATE_TX, initStateTxId)],
+            data: {
+                "Content-Type": "text/html",
+                body: "<h1>hello</h1>",
+            },
+            evaluationManifest: {
+                evaluationOptions: {
+                    useKVStorage: true,
+                },
+            },
+        },
+        true,
+    );
+
+    return deployment;
+}
 
 export async function deployContract<T extends ContractName>(
     warp: Warp,
@@ -55,38 +198,43 @@ export async function deployContract<T extends ContractName>(
         ? Erc1155State
         : T extends "scarcity"
         ? ScarcityState
-        : T extends "shuffle"
-        ? ShuffleState
-        : T extends "lock"
-        ? LockState
-        : never,
+        : // : T extends "shuffle"
+          // ? ShuffleState
+          // : T extends "lock"
+          // ? LockState
+          never,
 ) {
     const wasmDir = `../${contract}/implementation/pkg`;
     const wasmGluecode = `${wasmDir}/rust-contract.js`;
     const wasmPath = `${wasmDir}/rust-contract_bg.wasm`;
 
-    const deployment = await warp.createContract.deploy({
+    const deployment = await warp.deploy({
         wallet: opWallet,
         initState: JSON.stringify(initState),
         wasmSrcCodeDir: wasmDir,
         wasmGlueCode: wasmGluecode,
         src: readFileSync(wasmPath),
+        evaluationManifest: {
+            evaluationOptions: {
+                useKVStorage: true,
+            },
+        },
     });
 
     return deployment;
 }
 
-export type Interactor<ACTION, ERROR> = (
+export type Interactor<ACTION> = (
     interaction: ACTION,
     options?: { wallet?: JWKInterface } & WriteInteractionOptions,
-) => Promise<WriteInteractionResponse<ERROR> | null>;
+) => Promise<WriteInteractionResponse | null>;
 
-export function createInteractor<ACTION, ERROR>(
+export function createInteractor<ACTION>(
     warp: Warp,
-    contract: Contract<unknown, ERROR>,
+    contract: Contract<unknown>,
     defaultWallet: JWKInterface,
     defaultOptions: WriteInteractionOptions = {},
-): Interactor<ACTION, ERROR> {
+): Interactor<ACTION> {
     defaultOptions = { strict: true, ...defaultOptions };
 
     return async (
@@ -109,6 +257,24 @@ export function createInteractor<ACTION, ERROR>(
         await warp.testing.mineBlock();
 
         return interactionResult;
+    };
+}
+
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void
+    ? I
+    : never;
+
+export type Viewer<Action, Result, State> = ReturnType<typeof createViewer<Action, Result, State>>;
+
+export function createViewer<Action, Result, State>(contract: Contract<State>) {
+    type Results = UnionToIntersection<Result>;
+    type FunctionsWithResults = keyof Results;
+
+    return function view<T extends Action & { function: FunctionsWithResults }>(
+        action: T,
+        // ): Promise<InteractionResult<State, Results[T["function"]]>> {
+    ): Promise<InteractionResult<State, Pick<Results, T["function"]>>> {
+        return contract.viewState(action);
     };
 }
 
